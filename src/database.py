8ce4d63a7cce3d4496d2fc834423f70de53ee564
loristron <loremmiranda@gmail.com>
2022-05-11 21:37:52 -0400

    Change typo on column name

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 import string 
-import random
+from random import choices
 
 db = SQLAlchemy()
 
@@ -15,18 +15,6 @@ class User(db.Model):
     
     bookmarks = db.relationship('Bookmark', backref='user')
 
-    def generate_short_characters(self):
-        
-        characters = string.digits+string.ascii_letters
-        pick = ''.join(random.choices(characters, k=3))
-
-        link = self.query.filter_by(short_url=pick).first()
-        
-        if link: 
-            self.generate_short_characters() 
-        else:
-            return pick
-
     def __repr__(self) -> str:
         return 'User >>> {self.username}'
 
@@ -35,20 +23,28 @@ class Bookmark(db.Model):
     body = db.Column(db.Text(), nullable=True)
     url = db.Column(db.Text(), nullable=False)
     short_url = db.Column(db.String(3), nullable=False)
-    vistits = db.Column(db.Integer, default=0)
+    visits = db.Column(db.Integer, default=0)
     user_id = db.Column(db.ForeignKey('user.id'))
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, onupdate=datetime.now())
 
+    def generate_short_link(self):
+        characters = string.digits + string.ascii_letters
+        short_url = ''.join(choices(characters, k=3))
+
+        link = self.query.filter_by(short_url=short_url).first()
+
+        if link:
+            return self.generate_short_link()
+        
+        return short_url
+
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.short_url = self.generate_short_link()
 
     def __repr__(self) -> str:
         return 'Bookmark >>> {self.url}'
-
-    
-    def __init__(self, **kwargs) -> None:
-        super().__init__(**kwargs)
-        
-        self.short_url = self.generate_short_characters()
 
 
     
